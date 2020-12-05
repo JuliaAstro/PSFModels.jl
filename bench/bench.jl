ce858daba0c5d0c3ebda553b1160bfabac7f494b
Miles Lucas <mdlucas@hawaii.edu>
2020-12-05 05:19:36 -0600

    add benchmark

---
+++ bench/bench.jl
@@ -0,0 +1,42 @@
+using BenchmarkTools
+using CSV
+using DataFrames
+using PSFModels: Gaussian, AiryDisk, Moffat
+using PyCall
+
+apm = pyimport("astropy.modeling.functional_models")
+
+@info "Starting PSF evaluation benchmark"
+
+julia_models = [
+    Gaussian((5.3, 4.7)), 
+    AiryDisk(12.7), 
+    Moffat(8.1)
+]
+astropy_models = [
+    apm.Gaussian2D(x_stddev=5.3, y_stddev=4.7),
+    apm.AiryDisk2D(radius=12.7),
+    apm.Moffat2D(gamma=8.1, alpha=-1)
+]
+
+names = ("Gaussian", "AiryDisk", "Moffat")
+jl_times = []
+py_times = []
+
+for (jl_mod, py_mod, name) in zip(julia_models, astropy_models, names)
+    jl_time = @belapsed $jl_mod(0.5, 0.7)
+    push!(jl_times, jl_time)
+
+    py_time = @belapsed $py_mod(0.5, 0.7)
+    push!(py_times, py_time)
+    
+    @info "$name" PSFModels=jl_time astropy=py_time
+end
+
+filename = joinpath(@__DIR__, "results.csv")
+DataFrame(name=names, psfmodels=jl_times, astropy=py_times) |> CSV.write(filename)
+
+
+@info "Finished PSF evaluation benchmark. Results saved to $filename"
+
+nothing
