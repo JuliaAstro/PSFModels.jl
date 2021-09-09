dc64b6990155e14a12bd923df02ed8224fb1e7bf
Miles Lucas <mdlucas@hawaii.edu>
2021-09-09 13:25:09 -1000

    clean up the new Gaussian code

--- src/gaussian.jl
+++ src/gaussian.jl
@@ -1,8 +1,8 @@
 
 @doc raw"""
-    PSFModels.Gaussian([T=Float64]; fwhm, x=0, y=0, amp=1, maxsize=3, extent=maxsize .* fwhm)
-    PSFModels.Gaussian([T=Float64]; fwhm, pos=(0, 0), amp=1, maxsize=3, extent=maxsize .* fwhm)
-    PSFModels.Gaussian([T=Float64]; fwhm, r=0, theta=0, origin=(0, 0), amp=1, maxsize=3, extent=maxsize .* fwhm)
+PSFModels.Gaussian([T=Float64]; fwhm, x=0, y=0, amp=1, maxsize=3, extent=maxsize .* fwhm)
+PSFModels.Gaussian([T=Float64]; fwhm, pos=(0, 0), amp=1, maxsize=3, extent=maxsize .* fwhm)
+PSFModels.Gaussian([T=Float64]; fwhm, r=0, theta=0, origin=(0, 0), amp=1, maxsize=3, extent=maxsize .* fwhm)
 
 An unnormalized bivariate Gaussian distribution. The position can be specified in `(x, y)` coordinates as a `Tuple`, `AbstractVector`, or as separate arguments. By default the model is placed at the origin. The position can also be given as a polar coordinate using `r`/`ρ` and `theta`/`θ`.
 
@@ -20,17 +20,18 @@ struct Gaussian{T,FT,VT<:AbstractVector,IT<:Tuple} <: PSFModel{T}
     amp::T
     indices::IT
 
-    Gaussian(fwhm::FT, pos::VT, amp::T, indices::IT) where {T,FT,VT<:AbstractVector,IT<:Tuple} = new{T,FT,VT,IT}(fwhm,pos, amp, indices)
+    function Gaussian(fwhm::FT, pos::VT, amp::T, indices::IT) where {T,FT,VT<:AbstractVector,IT<:Tuple}
+        new{T,FT,VT,IT}(fwhm, pos, amp, indices)
+    end
 end
 
 # Alias Normal -> Gaussian
 """
-    PSFModels.Normal
+PSFModels.Normal
 
 An alias for [`PSFModels.Gaussian`](@ref)
 """
 const Normal = Gaussian
-
 
 ## constructors
 # default type is Float64
@@ -39,7 +40,7 @@ Gaussian(; kwargs...) = Gaussian(Float64; kwargs...)
 function Gaussian(T; fwhm, amp=one(T), maxsize=3, extent = maxsize .* fwhm, position...)
     # get the position from keyword distpatch
     pos = _position(; position...)
-    return Gaussian(fwhm, pos, convert(T, amp), indices_from_extent(pos, extent))
+return Gaussian(fwhm, pos, convert(T, amp), indices_from_extent(pos, extent))
 end
 
 Base.size(g::Gaussian) = map(length, g.indices)
@@ -61,52 +62,3 @@ function (g::Gaussian{T,<:Union{Tuple,AbstractVector}})(point::AbstractVector) w
     val = g.amp * exp(GAUSS_PRE * Δ)
     return convert(T, val)
 end
-
-@doc raw"""
-    PSFModels.Gaussian([T=Float64], X::AbstractVector; kwargs...)
-
-The following canned models are predefined depending on the length of the input vector. These are meant to be used in optimization problems.
-
-| N | Parameters                  |
-|---|:----------------------------|
-| 1 | fwhm                        |
-| 2 | fwhm\_x, fwhm\_y            |
-| 3 | x, y, fwhm                  |
-| 4 | x, y, fwhm, amp             |
-| 5 | x, y, fwhm\_x, fwhm\_y, amp |
-"""
-Gaussian(T, X::AbstractVector; kwargs...) = Gaussian(T, X, length(X); kwargs...)
-Gaussian(X::AbstractVector; kwargs...) = Gaussian(Float64, X; kwargs...)
-
-# option 1: ifelse
-function Gaussian(T, X::AbstractVector, N; kwargs...)
-    if N == 1
-        Gaussian(T; fwhm=X[begin], kwargs...)
-    elseif N == 2
-        Gaussian(T; fwhm=X, kwargs...)
-    elseif N == 3
-        Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=X[begin + 2], kwargs...)
-    elseif N == 4
-        Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=X[begin + 2], amp=X[end], kwargs...)
-    elseif N == 5
-        Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=@view(X[begin + 2:begin + 3]), amp=X[end], kwargs...)
-    end
-end
-
-# # option 2: Val lookup
-# Gaussian(T, X::AbstractVector, N) = Gaussian(T, X, Val(N))
-# Gaussian(T, X::AbstractVector, ::Val{1}) = Gaussian(T; fwhm=X[begin])
-# Gaussian(T, X::AbstractVector, ::Val{2}) = Gaussian(T; fwhm=X)
-# Gaussian(T, X::AbstractVector, ::Val{3}) = Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=X[begin + 2])
-# Gaussian(T, X::AbstractVector, ::Val{4}) = Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=@view(X[begin + 2:end]))
-# Gaussian(T, X::AbstractVector, ::Val{5}) = Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=@view(X[begin + 2:begin + 3]), amp=X[end])
-
-# # option 3: dict closure lookup
-# const GAUSS_MODEL_DICT = Dict(
-#     1 => (T, X) -> Gaussian(T; fwhm=X[begin]),
-#     2 => (T, X) -> Gaussian(T; fwhm=X),
-#     3 => (T, X) -> Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=X[begin + 2]),
-#     4 => (T, X) -> Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=@view(X[begin + 2:end])),
-#     5 => (T, X) -> Gaussian(T; x=X[begin], y=X[begin + 1], fwhm=@view(X[begin + 2:begin + 3]), amp=X[end]),
-# )
-# Gaussian(T, X::AbstractVector, N) = GAUSS_MODEL_DICT[N](T, X)
