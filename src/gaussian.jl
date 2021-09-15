8e2db5e07d4a60a76e76ea81f153a8ab21cf6e37
Miles Lucas <mdlucas@hawaii.edu>
2021-09-15 11:04:47 -1000

    write up gradients for Gaussian PSF

--- src/gaussian.jl
+++ src/gaussian.jl
@@ -61,3 +61,51 @@ function (g::Gaussian{T,<:Union{Tuple,AbstractVector}})(point::AbstractVector) w
     val = g.amp * exp(GAUSS_PRE * Δ)
     return convert(T, val)
 end
+
+## gradients
+
+# isotropic
+function fgrad(g::Gaussian, point::AbstractVector)
+    f = g(point)
+
+    xdiff = first(point) - first(g.pos)
+    ydiff = last(point) - last(g.pos)
+    dfdpos = -2 * GAUSS_PRE * f / g.fwhm^2 .* SA[xdiff, ydiff]
+    dfdfwhm = -2 * GAUSS_PRE * f * (xdiff^2 + ydiff^2) / g.fwhm^3
+    dfdamp = f / g.amp
+    return f, dfdpos, dfdfwhm, dfdamp
+end
+
+# short printing
+Base.show(io::IO, g::Gaussian{T}) where {T} = print(io, "Gaussian{$T}(pos=$(g.pos), fwhm=$(g.fwhm), amp=$(g.amp))")
+
+# diagonal
+function fgrad(g::Gaussian{T,<:Union{Tuple,AbstractVector}}, point::AbstractVector) where T
+    f = g(point)
+
+    xdiff = first(point) - first(g.pos)
+    ydiff = last(point) - last(g.pos)
+    dfdpos = -2 * GAUSS_PRE * f .* SA[xdiff / first(g.fwhm)^2, ydiff / last(g.fwhm)^2]
+    dfdfwhm = -2 * GAUSS_PRE * f .* SA[xdiff^2 / first(g.fwhm)^3, ydiff^2 / last(g.fwhm)^3]
+    dfda = f / g.amp
+    return f, dfdpos, dfdfwhm, dfda
+end
+
+function frule((Δpsf, Δp), g::Gaussian, point::AbstractVector)
+    f, dfdpos, dfdfwhm, dfda = fgrad(g, point)
+    Δf = dot(dfdpos, Δpsf.pos) + dot(dfdfwhm, Δpsf.fwhm) + dfda * Δpsf.amp
+    Δf -= dot(dfdpos, Δp)
+    return f, Δf
+end
+
+function rrule(g::G, point::AbstractVector) where {G<:Gaussian}
+    f, dfdpos, dfdfwhm, dfda = fgrad(g, point)
+    function Gaussian_pullback(Δf)
+        ∂pos = dfdpos .* Δf
+        ∂fwhm = dfdfwhm .* Δf
+        ∂g = Tangent{G}(pos=∂pos, fwhm=∂fwhm, amp=dfda * Δf, indices=ZeroTangent())
+        ∂pos = dfdpos .* -Δf
+        return ∂g, ∂pos
+    end
+    return f, Gaussian_pullback
+end
