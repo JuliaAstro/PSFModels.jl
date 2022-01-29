99e4305e283d1608209f19103e3b2e00f088cfe0
mileslucas <mdlucas@hawaii.edu>
2022-01-29 11:22:14 -1000

    rewrite airydisk implementation

--- src/functions/airy.jl
+++ src/functions/airy.jl
@@ -1,12 +1,11 @@
 
 @doc raw"""
-    PSFModels.AiryDisk([T=Float64]; fwhm, x=0, y=0, amp=1, maxsize=3, extent=maxsize .* fwhm)
-    PSFModels.AiryDisk([T=Float64]; fwhm, pos, amp=1, maxsize=3, extent=maxsize .* fwhm)
-    PSFModels.AiryDisk([T=Float64]; fwhm, r, theta, amp=1, maxsize=3, extent=maxsize .* fwhm)
+    PSFModels.airydisk([T=Float64], point; x, y, fwhm, amp=1, theta=0)
+    PSFModels.airydisk([T=Float64], px, py; x, y, fwhm, amp=1, theta=0)
 
-An unnormalized Airy disk. The position can be specified in `(x, y)` coordinates as a `Tuple`, `AbstractVector`, or as separate arguments. By default the model is placed at the origin. The position can also be given as a polar coordinate using `r`/`ρ` and `theta`/`θ`, optionally centered around `origin`.
+An unnormalized Airy disk. The position can be specified in `(x, y)` coordinates as a `Tuple`, `AbstractVector`, or as separate arguments. If `theta` is given, the PSF will be rotated by `theta` degrees counter-clockwise from the x-axis.
 
-The `fwhm` can be a scalar (isotropic) or a vector/tuple (diagonal). For efficient calculations, we recommend using [StaticArrays](https://github.com/JuliaArrays/StaticArrays.jl). Here, `maxsize` is a multiple of the fwhm, and can be given as a scalar or as a tuple for each axis. The `extent` defines the bounding box for the model and is used for the default rendering size.
+The `fwhm` can be a scalar (isotropic) or a vector/tuple (diagonal). Keep in mind that `theta` has no effect for isotropic distributions and is degenerate with the `fwhm` parameters (i.e., theta=90 is the same as reversing the `fwhm` tuple)
 
 # Functional form
 
@@ -20,42 +19,42 @@ where `J₁` is the first-order Bessel function of the first kind and
 q ≈ π * r / (0.973 * FWHM)
 ```
 """
-struct AiryDisk{T,FT,VT<:AbstractVector,IT<:Tuple} <: PSFModel{T}
-    fwhm::FT
-    pos::VT
-    amp::T
-    indices::IT
+airydisk(T, px, py; x, y, fwhm, amp=one(T), theta=0) = convert(T, _airydisk(px, py, x, y, fwhm, amp, theta))
+airydisk(px, py; kwargs...) = airydisk(Float64, px, py; kwargs...)
+airydisk(point::BivariateLike; kwargs...) = airydisk(point...; kwargs...)
+airydisk(idx::CartesianIndex; kwargs...) = airydisk(idx.I; kwargs...)
 
-    function AiryDisk(::Type{T}, fwhm::FT, pos::VT, amp::T, indices::IT) where {T,VT<:AbstractVector,FT,IT<:Tuple}
-        new{T,FT,VT,IT}(fwhm, pos, amp, indices)
+# factor for scaling radius in terms of the fwhm
+const rz = 1.18677 * π / 3.8317059702075125
+
+# isotropic
+function _airydisk(px, py, x, y, fwhm, amp, theta)
+    # find offset from center
+    dx = px - x
+    dy = py - y
+    # rotate 
+    !iszero(theta) && @warn "isotropic airydisk is not affected by non-zero rotation angle $theta"
+    # unnormalized airydisk
+    r = sqrt(dx^2 + dy^2)
+    # short-circuit
+    iszero(r) && return amp
+    r /= (fwhm * rz)
+    return amp * (2 * besselj1(π * r) / (π * r))^2
+end
+
+# bivariate
+function _airydisk(px, py, x, y, fwhm::BivariateLike, amp, theta)
+    # find offset from center
+    dx = px - x
+    dy = py - y
+    # rotate
+    if !iszero(theta)
+        dx, dy = rotate_point(dx, dy, theta)
     end
-end
-
-## constructors
-# default type is Float64
-AiryDisk(; kwargs...) = AiryDisk(Float64; kwargs...)
-function AiryDisk(T; fwhm, amp=one(T), maxsize=3, extent = maxsize .* fwhm, position...)
-    # get the position from keyword distpatch
-    pos = _position(; position...)
-    return AiryDisk(T, fwhm, pos, convert(T, amp), indices_from_extent(pos, extent))
-end
-
-Base.size(a::AiryDisk) = map(length, a.indices)
-Base.axes(a::AiryDisk) = a.indices
-
-const rz = 3.8317059702075125 / π
-
-function (a::AiryDisk{T})(point::AbstractVector) where T
-    radius = a.fwhm * 1.18677
-    Δ = euclidean(point, a.pos)
-    r = Δ / (radius / rz)
-    val = ifelse(iszero(r), a.amp, a.amp * (2 * besselj1(π * r) / (π * r))^2)
-    return convert(T, val)
-end
-
-function (a::AiryDisk{T,<:Union{AbstractVector,Tuple}})(point::AbstractVector) where T
-    weights = SA[(rz / (first(a.fwhm) * 1.18677))^2, (rz / (last(a.fwhm) * 1.18677))^2]
-    r = weuclidean(point, a.pos, weights)
-    val = ifelse(iszero(r), a.amp, a.amp * (2 * besselj1(π * r) / (π * r))^2)
-    return convert(T, val)
+    # unnormalized airydisk
+    fwhmx, fwhmy = fwhm
+    r = sqrt((dx / (fwhmx * rz))^2 + (dy / (fwhmy * rz))^2)
+    # short-circuit
+    iszero(r) && return amp
+    return amp * (2 * besselj1(π * r) / (π * r))^2
 end
