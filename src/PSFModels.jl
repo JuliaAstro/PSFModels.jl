e148a1efc4b0b788f764be82fc6a3d13a94ab82d
cgarling <chris.t.garling@gmail.com>
2026-05-31 09:05:58 -0400

    Add `fwhm` `ellipse_bounds` functions

--- src/PSFModels.jl
+++ src/PSFModels.jl
@@ -38,7 +38,7 @@ Evaluate the PSF model at position `(x, y)`.
 function evaluate end
 
 """
-    centroid(model::AbstractPSFModel{T})::Tuple{T, T}
+    centroid(model::AbstractPSFModel{T}) → (x::T, y::T)
 Return the centroid of the PSF model as a tuple `(x, y)`;
 default implementation assumes the centroid is given by fields `x` and `y` in the model struct.
 """
@@ -58,18 +58,68 @@ default implementation assumes the integral is given by a field `flux` in the mo
 integral(model::AbstractPSFModel) = hasproperty(model, :flux) ? model.flux : error("Model does not have a `flux` field; either add one or implement `integral(model)` for this model type.")
 
 """
-    fit_deriv(model::AbstractPSFModel{T}, x::Real, y::Real)
-Returns the partial derivatives of the `model`
-with respect to the parameters.
+    fwhm(model::AbstractPSFModel) → (x_fwhm::T, y_fwhm::T)
+Return the full width at half maximum (FWHM) of the PSF model as a tuple `(x_fwhm, y_fwhm)` in the x and y directions. By default, this function checks for a single `fwhm` field and returns it for both axes, or separate `x_fwhm` and `y_fwhm` fields if they exist. Models with more complex definitions of FWHM should implement their own version of this function.
 """
-function fit_deriv end
+function fwhm(model::AbstractPSFModel)
+    if hasproperty(model, :fwhm)
+        return (model.fwhm, model.fwhm)
+    elseif hasproperty(model, :x_fwhm) && hasproperty(model, :y_fwhm)
+        return (model.x_fwhm, model.y_fwhm)
+    else
+        error("Model does not have `fwhm` or `x_fwhm` and `y_fwhm` fields; either add them or implement `fwhm(model)` for this model type.")
+    end
+end
 
 """
-    fit_hessian(model::AbstractPSFModel{T}, x::Real, y::Real)
-Returns the second partial derivatives of the `model`
-with respect to the parameters.
+    evaluate_fg(model::AbstractPSFModel{T}, x::Real, y::Real) → (f::T, G::SVector{T})
+Returns the model value `f` and partial derivatives of the `model`
+with respect to the parameters `G` at position `(x, y)`.
 """
-function fit_hessian end
+function evaluate_fg end
+
+"""
+    evaluate_fgh(model::AbstractPSFModel{T}, x::Real, y::Real) → (f::T, G::SVector{T}, H::SMatrix{T})
+Returns the model value `f`, partial derivatives `G`, and Hessian matrix `H` of the `model`
+with respect to the parameters at position `(x, y)`.
+"""
+function evaluate_fgh end
+
+"""
+    ellipse_bounds(a, b, θ)
+Returns the bounding box of an ellipse with semi-major axis `a`, 
+semi-minor axis `b`, and rotation angle `θ` (degrees CCW from x-axis).
+
+```jldoctest
+julia> using PSFModels: ellipse_bounds
+
+julia> ellipse_bounds(3, 2, 0)
+(3.0, 2.0)
+
+julia> ellipse_bounds(3, 2, 90)
+(2.0, 3.0)
+
+julia> round.(ellipse_bounds(3, 2, 45); digits=3)
+(2.55, 2.55)
+```
+"""
+function ellipse_bounds(a, b, θ)
+    θ = deg2rad(θ)
+    cosθ = cos(θ)
+    sinθ = sin(θ)
+    x_bound = hypot(a * cosθ, b * sinθ)
+    y_bound = hypot(a * sinθ, b * cosθ)
+    return x_bound, y_bound
+end
+
+function bounding_box(model::AbstractPSFModel, factor=5)
+    # default bounding box is 5x5 around centroid, but specific models can override this
+    x0, y0 = centroid(model)
+    FWHM = fwhm(model)
+    a, b = factor * FWHM[1], factor * FWHM[2]
+    dx, dy = ellipse_bounds(a, b, 0)
+    return (x0 - 2.5, x0 + 2.5), (y0 - 2.5, y0 + 2.5)
+end
 
 include("gaussian.jl")
 include("airy.jl")
